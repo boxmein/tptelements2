52852b003b4a132216a0916191332b4291d601eb
boxmein <wsdf294@gmail.com>
2015-02-07 22:54:51 +0200

    Added lzw tool here

---
+++ tools/lzw-tool.js
@@ -0,0 +1,70 @@
+// a handy tool to decompress/compress LZW
+// usage: node lzwtool.js <mode> <file>
+// <mode> = d for decompress
+// <mode> = c for compress
+// outputs to stdout
+
+
+// LZW-compress a string
+function lzw_encode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var out = [];
+    var currChar;
+    var phrase = data[0];
+    var code = 256;
+    for (var i=1; i<data.length; i++) {
+        currChar=data[i];
+        if (dict[phrase + currChar] != null) {
+            phrase += currChar;
+        }
+        else {
+            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+            dict[phrase + currChar] = code;
+            code++;
+            phrase=currChar;
+        }
+    }
+    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+    for (var i=0; i<out.length; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
+}
+ 
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var currChar = data[0];
+    var oldPhrase = currChar;
+    var out = [currChar];
+    var code = 256;
+    var phrase;
+    for (var i=1; i<data.length; i++) {
+        var currCode = data[i].charCodeAt(0);
+        if (currCode < 256) {
+            phrase = data[i];
+        }
+        else {
+           phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+        }
+        out.push(phrase);
+        currChar = phrase.charAt(0);
+        dict[code] = oldPhrase + currChar;
+        code++;
+        oldPhrase = phrase;
+    }
+    return out.join("");
+}
+
+var fs = require('fs');
+
+// compress
+if (process.argv[2].indexOf("c") === 0) {
+  process.stdout.write(lzw_encode(fs.readFileSync(process.argv[3])));
+}
+// Decompress
+else if (process.argv[2].indexOf("d") === 0) {
+  process.stdout.write(lzw_decode(fs.readFileSync(process.argv[3])));
+}
